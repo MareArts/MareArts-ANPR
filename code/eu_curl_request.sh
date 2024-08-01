27802d1ad5f1afef9c3ff4f7cb24738e84bf5818
MareArts <feelmare@daum.net>
2024-08-01 23:16:59 +0300

    Delete code directory

--- code/eu_curl_request.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-curl -X POST https://we303v9ck8.execute-api.eu-west-1.amazonaws.com/Prod/marearts_anpr_eu \
-     -H "Content-Type: image/jpeg" \
-     -H "x-api-key: your-api-key" \
-     -H "user-id: your-user-id" \
-     --data-binary "@./path/upload.jpg"
